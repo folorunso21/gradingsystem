b648f8996aa16407304ce57f6bbfd66abfccfdd1
folorunso21 <folorunsoolamilekan2006@gmail.com>
2023-05-20 14:57:28 +0100

    firstcommit

---
+++ main.js
@@ -0,0 +1,18 @@
+function result() {
+    var a = document.getElementById("Boy").value
+    var b = document.getElementById("Girl").value
+    var calculate = Math.round((Math.random() * 100))
+
+    document.getElementById("inp").innerHTML = `The calculation between ${a} and ${b} is ${calculate}%`
+
+}
+console.log(Math.round(3.26))
+console.log(Math.ceil(4.26))
+console.log(Math.floor())
+console.log(Math.sqrt(4))
+console.log(Math.pow(4, 2))
+console.log("inp")
+
+for(var i=0; 0<20; 0++) {
+console.log ("Hello")
+}
